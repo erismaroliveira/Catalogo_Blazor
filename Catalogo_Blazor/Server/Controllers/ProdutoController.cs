f02feb8ad8f2e1e9c7728a50b9dce65b1feb5f11
Erismar Oliveira <74121763+erismaroliveira@users.noreply.github.com>
2022-04-22 10:22:39 -0300

    Criando o controlador Produto

---
+++ Catalogo_Blazor/Server/Controllers/ProdutoController.cs
@@ -0,0 +1,59 @@
+﻿using Catalogo_Blazor.Server.Context;
+using Catalogo_Blazor.Shared.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Catalogo_Blazor.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutoController : ControllerBase
+    {
+        private readonly AppDbContext context;
+
+        public ProdutoController(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Produto>>> Get()
+        {
+            return await context.Produtos.AsNoTracking().ToListAsync();
+        }
+
+        [HttpGet("{id}", Name = "GetProduto")]
+        public async Task<ActionResult<Produto>> Get(int id)
+        {
+            return await context.Produtos.FirstOrDefaultAsync(x => x.ProdutoId == id);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Produto>> Post(Produto produto)
+        {
+            context.Add(produto);
+            await context.SaveChangesAsync();
+            return new CreatedAtRouteResult("GetProduto", new { id = produto.ProdutoId }, produto);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<Produto>> Put(Produto produto)
+        {
+            context.Entry(produto).State = EntityState.Modified;
+            await context.SaveChangesAsync();
+            return Ok(produto);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Produto>> Delete(int id)
+        {
+            var produto = new Produto { ProdutoId = id};
+            context.Remove(produto);
+            await context.SaveChangesAsync();
+            return Ok(produto);
+        }
+    }
+}
